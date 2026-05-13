9db5b0202d93b70f21661b082ba475fe4b79da56
Nilesh Sahu <55877103+nileshmsd12345@users.noreply.github.com>
2026-05-13 11:37:49 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.kt
@@ -0,0 +1,25 @@
+class Solution {
+fun isValid(s: String): Boolean {
+
+        val map = mapOf(
+            "(" to ")", "{" to "}", "[" to "]"
+        )
+
+        val stack = ArrayDeque<String>()
+
+        for(a in s){
+            val c = a.toString()
+            if(map.containsKey(c)){
+                stack.addLast(c)
+            }else{
+                if(stack.isEmpty || map[stack.removeLast()]!=c)
+{
+    return false
+}
+            
+
+            }
+        }
+        return stack.isEmpty()
+    }
+}
