cd289a5c8df25423f7ff552eba2b7f238af03384
Nilesh Sahu <55877103+nileshmsd12345@users.noreply.github.com>
2026-05-19 12:08:03 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+
+        val map = mutableMapOf<Int, Int>()
+
+        for (i in nums.indices) {
+
+            val complement = target - nums[i]
+
+            if (map.containsKey(complement)) {
+                return intArrayOf(map[complement]!!, i)
+            }
+
+            map[nums[i]] = i
+        }
+
+        return IntArray(0)
+    }
+}
