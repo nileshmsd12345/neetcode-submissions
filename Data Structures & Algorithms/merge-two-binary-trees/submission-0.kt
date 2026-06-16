dda6fc845afa8c3832ba72627301b862ed8e6f67
Nilesh Sahu <55877103+nileshmsd12345@users.noreply.github.com>
2026-06-16 14:11:17 +0530

    Add: merge-two-binary-trees - submission-0

---
+++ Data Structures & Algorithms/merge-two-binary-trees/submission-0.kt
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun mergeTrees(root1: TreeNode?, root2: TreeNode?): TreeNode? {
+      if(root1== null && root2== null)return null
+      
+       val root: TreeNode  =  TreeNode((root1?.`val` ?: 0) + (root2?.`val` ?: 0))
+
+      root.left = mergeTrees(root1?.left, root2?.left)
+       root.right = mergeTrees(root1?.right, root2?.right)
+     return root
+    }
+
+  
+}
